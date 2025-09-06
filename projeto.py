20a30c604d9a4ea54372bc7bd209cfb976041911
michellewin <michel.lewin@hotmail.com>
2025-09-05 21:34:49 -0300

    atualização da branch

--- projeto.py
+++ projeto.py
@@ -2,4 +2,4 @@ versao inicial
 versao alterada no github
 versao alterada no github na nova branch
 versão alterada no gitlocal novabranchlocal
-alteração Michel
+alteração Michel na branch do git hub 050925
